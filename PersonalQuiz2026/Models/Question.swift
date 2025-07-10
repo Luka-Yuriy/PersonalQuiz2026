5ad751c4c9d129a44bb1818b42ff3431bb1ce084
Yuri Luka <lukayurii.work@gmail.com>
2025-07-10 19:14:33 +0500

    Question logic

---
+++ PersonalQuiz2026/Models/Question.swift
@@ -0,0 +1,80 @@
+//
+//  Question.swift
+//  PersonalQuiz2026
+//
+//  Created by Yurii Luka on 10.07.2025.
+//
+
+struct Question {
+    let title: String
+    let type: ResponseType
+    let answers: [Answer]
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let type: AnimalType
+}
+
+enum AnimalType: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравится быть с друзьями"
+        case .cat:
+            return "Вы сам себе на уме"
+        case .rabbit:
+            return "Вам нравитс все мягкое"
+        case .turtle:
+            return "Ваша сила - в мудрости"
+        }
+    }
+}
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", type: .dog),
+                    Answer(title: "Рыба", type: .cat),
+                    Answer(title: "Морковь", type: .rabbit),
+                    Answer(title: "Кукуруза", type: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", type: .dog),
+                    Answer(title: "Спать", type: .cat),
+                    Answer(title: "Обниматься", type: .rabbit),
+                    Answer(title: "Есть", type: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", type: .cat),
+                    Answer(title: "Нервничаю", type: .rabbit),
+                    Answer(title: "Не замечаю", type: .turtle),
+                    Answer(title: "Обожаю", type: .dog)
+                ]
+            ),
+        ]
+    }
+}
