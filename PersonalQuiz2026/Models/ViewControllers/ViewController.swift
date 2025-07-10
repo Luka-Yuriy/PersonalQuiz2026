40697de10f65791367e026cb99a6019130f7aa8b
Yuri Luka <lukayurii.work@gmail.com>
2025-07-10 17:27:20 +0500

    MVC folders

---
+++ PersonalQuiz2026/Models/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz2026
+//
+//  Created by Yurii Luka on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
