b88ec83ac96181c2f77ec0cf2c61e048464b1417
Yuri Luka <lukayurii.work@gmail.com>
2025-07-14 15:54:37 +0500

    Rework logic

--- PersonalQuiz2026/ViewControllers/ResultViewController.swift
+++ PersonalQuiz2026/ViewControllers/ResultViewController.swift
@@ -13,33 +13,39 @@ class ResultViewController: UIViewController {
     @IBOutlet var definitionLabel: UILabel!
     
     var answersChosen: [Answer]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
-        getResult()
+        updateResult()
     }
-
+    
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
     }
+}
     
-    private func getResult() {
-        var animalChosen: [AnimalType] = []
+//MARK: Private Methods
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        let animals = answersChosen.map { $0.type }
         
-        for answer in answersChosen {
-            animalChosen.append(answer.type)
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+            
+            let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+            guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+            
+            updateUI(with: mostFrequencyAnimal)
         }
-        
-        var frequencyDict: [AnimalType : Int] = [:]
-        for animal in animalChosen {
-            frequencyDict[animal, default: 0] += 1
-        }
-        
-        guard let (mostFrequentElement, _) = frequencyDict.max(by: { $0.value < $1.value }) else { return }
-        let mostCommonAnimal = mostFrequentElement
-        
-        resultLabel.text = "Вы - \(mostCommonAnimal.rawValue)"
-        definitionLabel.text = mostCommonAnimal.definition
+    }
+    private func updateUI(with animal: AnimalType) {
+        resultLabel.text = "Вы - \(animal.rawValue)!"
+        definitionLabel.text = animal.definition
     }
 }
